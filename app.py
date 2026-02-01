bb2d23ddac561344ce79abb585e8699a25fc62d8
adarshsharma020101 <adarsh2023as@gmail.com>
2026-02-01 09:50:34 +0530

    Enhance BMI prediction with category feature
    Added BMI category calculation and updated data input for prediction.

--- app.py
+++ app.py
@@ -8,19 +8,37 @@ app = Flask(__name__)
 model = joblib.load("bmi_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+
     prediction = ""
 
     if request.method == "POST":
+
         height = float(request.form["height"])
         weight = float(request.form["weight"])
 
+        # Calculate BMI
         bmi = weight / (height ** 2)
 
-        data = np.array([[bmi]])
+        # Create BMI category (same as training)
+        if bmi < 18.5:
+            bmi_cat = 0
+        elif bmi < 25:
+            bmi_cat = 1
+        elif bmi < 30:
+            bmi_cat = 2
+        else:
+            bmi_cat = 3
+
+        # IMPORTANT: 2 FEATURES
+        data = np.array([[bmi, bmi_cat]])
+
+        # Scale
         data = scaler.transform(data)
 
+        # Predict
         result = model.predict(data)
 
         if result[0] == 1:
@@ -30,5 +48,8 @@ def home():
 
     return render_template("index.html", prediction=prediction)
 
+
 if __name__ == "__main__":
+    app.run(debug=True)
+
     app.run(debug=True)
